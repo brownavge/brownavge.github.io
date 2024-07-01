ba12b3bad0d2ba2660788f98927a0c816fc2dfc5
Emily Wang <emilywang188@gmail.com>
2024-07-01 15:53:10 -0400

    landing page layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,28 @@
 import React from 'react';
-
+import guitars from'../images/guitars.jpg';
+ 
 
 const Home = () => {
+  // const navbar = document.querySelector('.navbar');
+  // let top = navbar.offsetTop;
+
+  // function stickynavbar() {
+  //   if (window.scrollY >= top) {    
+  //     navbar.classList.add('sticky');
+  //   } else {
+  //     navbar.classList.remove('sticky');    
+  //   }
+  // }
+  // window.addEventListener('scroll', stickynavbar);
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'centre',
-        alignItems: 'centre',
-        height: '100vh'
-      }}
-    >
-      <h1>Home</h1>
+    <div style={{ position: 'relative', height: '100vh', overflow: 'hidden' }}>
+      <img src={guitars} alt="guitars.jpg" />
+      <div class="overlay"></div>
+      <div className="centered">
+        <p style={{ fontSize: '20px' }}>BROWN UNIVERSITY X RISD'S</p>
+        <p style={{ fontSize: '50px' }}><b>ANIME VIDEO GAME ENSEMBLE</b></p>
+      </div>
+      
       
     </div>
   );
