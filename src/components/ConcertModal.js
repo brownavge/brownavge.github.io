de53bd77680ad1ab44654395fae9a6705e3aa715
Emily Wang <emilywang188@gmail.com>
2024-07-11 16:59:15 -0400

    styling event modals

--- src/components/ConcertModal.js
+++ src/components/ConcertModal.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Carousel } from 'react-bootstrap';
 import '../css/style.css';
 
 const ConcertModal = ({ show, handleClose, concert }) => {
   return (
-    <Modal show={show} onHide={handleClose}>
+    <Modal show={show} onHide={handleClose} dialogClassName="custom-modal" className="wide-modal">
       <Modal.Header closeButton>
-        <Modal.Title>{concert.title}</Modal.Title>
+
       </Modal.Header>
       <Modal.Body>
-        <div style={{ display: 'flex', width: '300px'}}>
-        <img src={concert.image} alt={concert.title} style={{ width: '20%', height: 'auto' }} />
-        <p>{concert.description}</p>
+        <div style={{ display: 'flex', gap: '10px' }}>
+          <div style={{ borderRight: '1px dotted gray', padding: '20px', width: '30%', display: 'flex', flexDirection: 'column', gap: '10px' }}>
+            <Modal.Title><center>{concert.title}</center></Modal.Title>
+            <img src={concert.image} alt={concert.title} style={{ width: 'auto' }} />
+            <center><a href={concert.setlist} rel="noreferrer" target='_blank'>Setlist</a></center>
+          </div>
+          <div style={{ overflowY: 'scroll', height: '60vh', padding: '10px', display: 'flex', flexDirection: 'column', gap: '10px', width: '100%' }}>
+            <p style={{ fontSize: '20px' }}><center>{concert.description}</center></p>
+            <p style={{ fontSize: '20px' }}><center>Small Ensembles</center></p>
+            <Carousel interval={null}>
+              {concert.matineeSmalls.map((video, index) => (
+                <Carousel.Item key={index}>
+                  <iframe
+                    width="100%"
+                    height="400"
+                    src={`https://www.youtube.com/embed/${video}`}
+                    title={`YouTube video player ${index}`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    loading="lazy"
+                    allowFullScreen
+                  ></iframe>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+            <Carousel interval={null}>
+              {concert.eveningSmalls.map((video, index) => (
+                <Carousel.Item key={index}>
+                  <iframe
+                    width="100%"
+                    height="400"
+                    src={`https://www.youtube.com/embed/${video}`}
+                    title={`YouTube video player ${index}`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    loading="lazy"
+                    allowFullScreen
+                  ></iframe>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </div>
         </div>
       </Modal.Body>
       <Modal.Footer>
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import ConcertModal from '../components/ConcertModal.js'; 
+import ConcertModal from '../components/ConcertModal.js';
 import Fall22 from '../images/poster_f22.jpg';
 import Spring23 from '../images/poster_s23.jpg';
 import Fall23 from '../images/poster_f23.png';
 import Spring24 from '../images/poster_s24.png';
+import Spring22 from '../images/poster_s22.jpg';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/style.css';
+import richard_talking from '../images/richard_talking.jpg';
 
 const Events = () => {
   const [showModal, setShowModal] = useState(false);
@@ -13,28 +16,45 @@ const Events = () => {
 
   const concerts = [
     {
-      id: 3,
-      title: 'Fall 2023 Concert',
-      image: Fall23,
-      description: 'Details about Concert 2...'
-    },
-    {
       id: 4,
       title: 'Spring 2024 Concert',
       image: Spring24,
-      description: 'Details about Concert 2...'
+      description: 'Monday, May 6, 2024 in Riley Hall',
+      matineeSmalls: ['K719Z9QRDr8', 'cVpCxgUqSGs', 'BPqMePeV8Sc', 'fYLY-NQY8y0', 'eHwqwFtnTs4', 'IBJ7-ShWZcc', 'ozPT0On_7SU', 'cI4D9qmFHrc', 'vveX6BgB_Ho', '20U5Mp36Cws', 'zUx4pz82E7o'],
+      eveningSmalls: ['zx5L6pGXv2g', 'pwliKfkFT3A', '4ttQkWiVF54', 'Pv9Ai7npyPk', 'NGPSXyQsnEk', '1lc5fFcBSag', 'jN06WQWWwsI', 'YeygveYOzIw', 'vSBNWKse4bg', 'Jidz-pQEMvg', 'qszbZh_xFWQ'],
+      setlist: 'https://drive.google.com/file/d/1FJW-5aob0H9rFSMuf-Our36e84pxX8qB/view?usp=drive_link'
     },
     {
-      id: 1,
-      title: 'Fall 2022 Concert',
-      image: Fall22,
-      description: 'Details about Concert 1...'
+      id: 3,
+      title: 'Fall 2023 Concert',
+      image: Fall23,
+      description: "Saturday, December 2 in Riley Hall",
+      matineeSmalls: [],
+      eveningSmalls: []
     },
     {
       id: 2,
       title: 'Spring 2023 Concert',
       image: Spring23,
-      description: 'Details about Concert 2...'
+      description: 'Friday, April 28 in Alumnae Hall',
+      matineeSmalls: [],
+      eveningSmalls: []
+    },
+    {
+      id: 1,
+      title: 'Fall 2022 Concert',
+      image: Fall22,
+      description: 'Monday, December 5 in Alumnae Hall',
+      matineeSmalls: [],
+      eveningSmalls: []
+    },
+    {
+      id: 0,
+      title: 'Spring 2022 Concert',
+      image: Spring22,
+      description: 'Sunday, April 17 in Petteruti Lounge',
+      matineeSmalls: [],
+      eveningSmalls: []
     },
   ];
 
@@ -49,23 +69,30 @@ const Events = () => {
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-      <h1>Events</h1>
-      <div className="poster-container">
-        {concerts.map(concert => (
-          <div key={concert.id} className="poster-item" onClick={() => handlePosterClick(concert)} style={{ cursor: 'pointer' }}>
-            <img src={concert.image} alt={concert.title} style={{ width: '100%', height: 'auto' }} />
-            <p>{concert.title}</p>
-          </div>
-        ))}
+    <div>
+      <div style={{ position: 'relative', height: '20vh', overflow: 'hidden' }}>
+        <img className='background-img' style={{ objectPosition: '50% 400%' }} src={richard_talking} alt="Guitars" />
+        <div className="overlay"></div>
+        <div className="centered">
+          <p style={{ fontSize: '50px', marginTop: '30%' }}>EVENTS</p>
+        </div>
       </div>
-      {selectedConcert && (
-        <ConcertModal
-          show={showModal}
-          handleClose={handleCloseModal}
-          concert={selectedConcert}
-        />
-      )}
+      <div style={{ padding: '2%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', backgroundColor: 'black' }}>
+        <div className="poster-container">
+          {concerts.map(concert => (
+            <div key={concert.id} className="poster-item" onClick={() => handlePosterClick(concert)} style={{ cursor: 'pointer' }}>
+              <img src={concert.image} alt={concert.title} style={{ width: '100%', height: 'auto' }} />
+            </div>
+          ))}
+        </div>
+        {selectedConcert && (
+          <ConcertModal
+            show={showModal}
+            handleClose={handleCloseModal}
+            concert={selectedConcert}
+          />
+        )}
+      </div>
     </div>
   );
 };
