95dc2feec1c8c95f1fac6508229c5ca8262506e9
Emily Wang <emilywang188@gmail.com>
2024-07-17 11:38:43 -0400

    final draft s24 modal

--- src/components/ConcertModal.js
+++ src/components/ConcertModal.js
@@ -5,49 +5,106 @@ import '../css/style.css';
 const ConcertModal = ({ show, handleClose, concert }) => {
   return (
     <Modal show={show} onHide={handleClose} dialogClassName="custom-modal" className="wide-modal">
-      <Modal.Header closeButton>
-
-      </Modal.Header>
+      <Modal.Header closeButton></Modal.Header>
       <Modal.Body>
         <div style={{ display: 'flex', gap: '10px' }}>
           <div style={{ borderRight: '1px dotted gray', padding: '20px', width: '30%', display: 'flex', flexDirection: 'column', gap: '10px' }}>
-            <Modal.Title><center>{concert.title}</center></Modal.Title>
-            <img src={concert.image} alt={concert.title} style={{ width: 'auto' }} />
-            <center><a href={concert.setlist} rel="noreferrer" target='_blank'>Setlist</a></center>
+            <Modal.Title><center>{concert?.title}</center></Modal.Title>
+            <img src={concert?.image} alt={concert?.title} style={{ width: 'auto' }} />
+            <center><a href={concert?.setlist} rel="noreferrer" target='_blank'>Setlist</a></center>
           </div>
           <div style={{ overflowY: 'scroll', height: '60vh', padding: '10px', display: 'flex', flexDirection: 'column', gap: '10px', width: '100%' }}>
-            <p style={{ fontSize: '20px' }}><center>{concert.description}</center></p>
-            <p style={{ fontSize: '20px' }}><center>Small Ensembles</center></p>
-            <Carousel interval={null}>
-              {concert.matineeSmalls.map((video, index) => (
-                <Carousel.Item key={index}>
-                  <iframe
-                    width="100%"
-                    height="400"
-                    src={`https://www.youtube.com/embed/${video}`}
-                    title={`YouTube video player ${index}`}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    loading="lazy"
-                    allowFullScreen
-                  ></iframe>
-                </Carousel.Item>
-              ))}
-            </Carousel>
-            <Carousel interval={null}>
-              {concert.eveningSmalls.map((video, index) => (
-                <Carousel.Item key={index}>
-                  <iframe
-                    width="100%"
-                    height="400"
-                    src={`https://www.youtube.com/embed/${video}`}
-                    title={`YouTube video player ${index}`}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    loading="lazy"
-                    allowFullScreen
-                  ></iframe>
-                </Carousel.Item>
-              ))}
-            </Carousel>
+            <p style={{ fontSize: '20px', marginBottom: '0px' }}><center>{concert?.date}</center></p>
+            <p style={{ fontSize: '13px', marginTop: '0px' }}><center><i>{concert?.description}</i></center></p>
+            <hr></hr>
+            
+            {concert?.matineeSmalls?.length > 0 && (
+              <>
+                <p style={{ fontSize: '20px', marginBottom: '0px' }}><center>Matinee Small Ensembles</center></p>
+                <p style={{ fontSize: '13px', marginTop: '0px', marginBottom: '5px' }}><center>{concert?.matineeList}</center></p>
+                <Carousel interval={null}>
+                  {concert.matineeSmalls.map((video, index) => (
+                    <Carousel.Item key={index}>
+                      <iframe
+                        width="100%"
+                        height="400"
+                        src={`https://www.youtube.com/embed/${video}`}
+                        title={`YouTube video player ${index}`}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        loading="lazy"
+                        allowFullScreen
+                      ></iframe>
+                    </Carousel.Item>
+                  ))}
+                </Carousel>
+              </>
+            )}
+
+            {concert?.eveningSmalls?.length > 0 && (
+              <>
+              <p style={{ fontSize: '20px', marginTop: '30px', marginBottom: '0px' }}><center>Evening Small Ensembles</center></p>
+              <p style={{ fontSize: '13px', marginTop: '0px', marginBottom: '5px' }}><center>{concert?.eveningList}</center></p>
+              <Carousel interval={null}>
+                {concert.eveningSmalls.map((video, index) => (
+                  <Carousel.Item key={index}>
+                    <iframe
+                      width="100%"
+                      height="400"
+                      src={`https://www.youtube.com/embed/${video}`}
+                      title={`YouTube video player ${index}`}
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      loading="lazy"
+                      allowFullScreen
+                    ></iframe>
+                  </Carousel.Item>
+                ))}
+              </Carousel>
+              <hr></hr>
+              </>
+            )}
+
+            {concert?.large?.length > 0 && (
+              <>
+                <p style={{ fontSize: '20px', marginBottom: '0px' }}><center>Large Ensemble</center></p>
+                <p style={{ fontSize: '13px', marginTop: '0px', marginBottom: '5px' }}><center>{concert?.largeList}</center></p>
+                <Carousel interval={null}>
+                  {concert.large.map((video, index) => (
+                    <Carousel.Item key={index}>
+                      <iframe
+                        width="100%"
+                        height="400"
+                        src={`https://www.youtube.com/embed/${video}`}
+                        title={`YouTube video player ${index}`}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        loading="lazy"
+                        allowFullScreen
+                      ></iframe>
+                    </Carousel.Item>
+                  ))}
+                </Carousel><hr></hr>
+              </>
+            )}
+
+            {concert?.full?.length > 0 && (
+              <>
+              <p style={{ fontSize: '20px', marginBottom: '0px' }}><center>Full Concert Tapes</center></p>
+              <Carousel interval={null}>
+                {concert.full.map((video, index) => (
+                  <Carousel.Item key={index}>
+                    <iframe
+                      width="100%"
+                      height="400"
+                      src={`https://www.youtube.com/embed/${video}`}
+                      title={`YouTube video player ${index}`}
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      loading="lazy"
+                      allowFullScreen
+                    ></iframe>
+                  </Carousel.Item>
+                ))}
+              </Carousel>
+              </>
+            )}
           </div>
         </div>
       </Modal.Body>
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -5,6 +5,7 @@ import Spring23 from '../images/poster_s23.jpg';
 import Fall23 from '../images/poster_f23.png';
 import Spring24 from '../images/poster_s24.png';
 import Spring22 from '../images/poster_s22.jpg';
+import F23Program from '../images/program_f23.pdf';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/style.css';
@@ -19,42 +20,76 @@ const Events = () => {
       id: 4,
       title: 'Spring 2024 Concert',
       image: Spring24,
-      description: 'Monday, May 6, 2024 in Riley Hall',
+      date: 'Monday, May 6, 2024 in Riley Hall',
+      description: "AVGE's largest concert to date, featuring twenty-four small ensembles and over 80 unique performers. A big thank you to Brown Ground Breakin', who featured in a collaboration in a 7pm Small Ensemble. This concert stood out as a uniquely sentimental one, as we said goodbye to a record number (16!) of graduating seniors and grad students, including our very own Johann Dizon, one of the founders of AVGE.",
+      matineeList: 'Weight of the World (NieR: Automata) • Fire Emblem Map Medley • Shitsuren, Sincerely (Summertime Rendering, Violet Evergarden) • Is There Still Anything That Love Can Do? (Weathering With You) • Tonight Stars an Easygoing Egoist (Touhou 15) • Genshin Impact Medley • Fly, My Wings (Limbus Company) • Arknights Medley • Innocence (Katawa Shojo) • Country Roads (Whisper of the Heart) • Unravel (Tokyo Ghoul)',
       matineeSmalls: ['K719Z9QRDr8', 'cVpCxgUqSGs', 'BPqMePeV8Sc', 'fYLY-NQY8y0', 'eHwqwFtnTs4', 'IBJ7-ShWZcc', 'ozPT0On_7SU', 'cI4D9qmFHrc', 'vveX6BgB_Ho', '20U5Mp36Cws', 'zUx4pz82E7o'],
+      eveningList: "Shelter • Anytime Anywhere (Frieren) • Fireworks Festival (Weathering With You) • Yuri on Ice • Never Forget (Bocchi The Rock!) • Message of Rouge (Kiki's Delivery Service) • Gwyn Lord of Cynder (Dark Souls) • Mute City (Mario Kart 8) • Bitter Eyes (Flip of Light) • Baka Mitai (Yakuza 0) • Lost in Paradise (Jujutsu Kaisen)",
       eveningSmalls: ['zx5L6pGXv2g', 'pwliKfkFT3A', '4ttQkWiVF54', 'Pv9Ai7npyPk', 'NGPSXyQsnEk', '1lc5fFcBSag', 'jN06WQWWwsI', 'YeygveYOzIw', 'vSBNWKse4bg', 'Jidz-pQEMvg', 'qszbZh_xFWQ'],
+      largeList: 'Journey of a Lifetime (Frieren) • Dearly Beloved (Kingdom Hearts) • Selections from Tears of the Kingdom (Legend of Zelda: TOTK) • Kingdom of Predators (Hunter X Hunter) • Hopes and Dreams (Undertale) • T-kt (Attack on Titan) • God Shattering Star (Fire Emblem: Three Houses) • My Neighbor Totoro',
+      large: ['x1SRUJCB2XE', 'zPKk89_tPIU', 'gx7VG4nKUsM', 'P89ArVNOJa8', 'PnZHHoN85xU', '3wxPOQDEwmk', 'hNwU0cvZmt4', 'UtRBCSPJytI', '7LxQ_E9xR1k'],
+      full: ['kcfWnKEZGXk', 'f9DrHikQUvk'],
       setlist: 'https://drive.google.com/file/d/1FJW-5aob0H9rFSMuf-Our36e84pxX8qB/view?usp=drive_link'
     },
     {
       id: 3,
       title: 'Fall 2023 Concert',
       image: Fall23,
-      description: "Saturday, December 2 in Riley Hall",
+      date: "Saturday, December 2 in Riley Hall",
+      description: "AVGE's first performance in Riley Hall of the Lindemann Performing Arts Center at Brown. This concert marked the beginning of many things; it featured our first use of projector media and our first collaboration, that with Gendo Taiko in one of our Large Ensemble pieces. This was also our first concert series with separate matinee and evening performances due to an overwhelming surge in small ensemble registration as well as audience support.",
+      matineeList: '',
       matineeSmalls: [],
-      eveningSmalls: []
+      eveningList: '',
+      eveningSmalls: [],
+      largeList: 'U.N. Owen Was Her? (Touhou 6) • Old Stories (Made in Abyss) • Castle in the Sky Medley • Violet Evergarden Medley • Inazuma (Genshin Impact) • Staff Roll (Super Mario Galaxy)',
+      large: [],
+      full: [],
+      setlist: F23Program
     },
     {
       id: 2,
       title: 'Spring 2023 Concert',
       image: Spring23,
-      description: 'Friday, April 28 in Alumnae Hall',
+      date: 'Friday, April 28 in Alumnae Hall',
+      description: "The birthplace of the iconic quote, 'This is the end of your merry-go-round of life... of college.' This semester also saw the start of AVGE's arranging team.",
+      matineeList: '',
       matineeSmalls: [],
-      eveningSmalls: []
+      eveningList: '',
+      eveningSmalls: [],
+      largeList: '',
+      large: [],
+      full: [],
+      setlist: ''
     },
     {
       id: 1,
       title: 'Fall 2022 Concert',
       image: Fall22,
-      description: 'Monday, December 5 in Alumnae Hall',
+      date: 'Monday, December 5 in Alumnae Hall',
+      description: "AVGE's first concert as an official ensemble. With a grand total of twenty performers across small and large ensemble, this group helped propel the club towards a bright and hopeful future!",
+      matineeList: '',
       matineeSmalls: [],
-      eveningSmalls: []
+      eveningList: '',
+      eveningSmalls: [],
+      largeList: '',
+      large: [],
+      full: [],
+      setlist: ''
     },
     {
       id: 0,
       title: 'Spring 2022 Concert',
       image: Spring22,
-      description: 'Sunday, April 17 in Petteruti Lounge',
+      date: 'Sunday, April 17 in Petteruti Lounge',
+      description: "AVGE's first semester of existence. This tight-knit group of six - Johann, Michelle, Richard, Renee, Ryan, and Sophia - began meeting weekly in the Steinert basement, and formed AVGE's first ensemble.",
+      matineeList: '',
       matineeSmalls: [],
-      eveningSmalls: []
+      eveningList: '',
+      eveningSmalls: [],
+      largeList: '',
+      large: [],
+      full: [],
+      setlist: ''
     },
   ];
 
